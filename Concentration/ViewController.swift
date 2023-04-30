8c27d573291da8ee4644eaa303e7eddd9d19096b
Egor Ukolov <ukolovegor@yandex.ru>
2023-04-30 12:17:59 +0700

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Egor Ukolov on 30.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
